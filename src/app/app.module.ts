93c494003e5e23684c9483b3645b97f990a2c205
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-19 16:24:11 +0800

    Completed: 013 A Basic Project Setup using Bootstrap for Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
+// import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    // FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
