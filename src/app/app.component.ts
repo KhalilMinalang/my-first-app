513364e5fb3ca104a76da36f9b414427ce806a7e
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-19 15:41:11 +0800

    Completed: 008 Editing the First App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // tag name
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-app';
+  // data binding
+  name = 'Max';
 }
