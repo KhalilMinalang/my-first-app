f47b38f1ebec1f9f86d7e95fd95dcd389a3709de
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-21 09:04:47 +0800

    Completed: 019 Understanding the Role of AppModule and Component Declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // components must be imported first before declaring
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    // components must be declared here in order for angular to recognize it
+    ServerComponent,
+  ],
   imports: [
     BrowserModule,
     // FormsModule
