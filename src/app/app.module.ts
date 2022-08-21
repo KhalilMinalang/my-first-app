139a6421a8ec8d1e31e38b19aff1074a283e44b7
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-21 11:53:41 +0800

    Completed: 025 [assignment_solution] Practicing Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,17 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component'; // components must be imported first before declaring
 
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     // components must be declared here in order for angular to recognize it
     ServerComponent,
     ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
   ],
   imports: [
     BrowserModule,
