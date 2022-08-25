e3f6f1c86c961592794f1ac7c3b0a43c29a633aa
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-25 20:16:12 +0800

    Completed: 073 More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,13 +1,17 @@
 import {
   Component,
   OnInit,
-  Input, // must be imported
+  Input,
+  ViewEncapsulation, // must be imported
 } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // default
+  // encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ServerElementComponent implements OnInit {
   // type definition
