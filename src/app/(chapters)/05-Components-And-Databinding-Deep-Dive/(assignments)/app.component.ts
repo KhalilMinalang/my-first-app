f1d04cbcf0d6824e6773a8732f109d3a31602d5c
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-28 16:46:58 +0800

    Completed: 091 Module Introduction

--- src/app/(chapters)/05-Components-And-Databinding-Deep-Dive/(assignments)/app.component.ts
+++ src/app/(chapters)/05-Components-And-Databinding-Deep-Dive/(assignments)/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // tag name
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   oddNumbers: number[] = [];
@@ -12,8 +13,10 @@ export class AppComponent {
   onIntervalFired(firedNumber: number) {
     if (firedNumber % 2 === 0) {
       this.evenNumbers.push(firedNumber);
+      console.log(this.evenNumbers);
     } else {
       this.oddNumbers.push(firedNumber);
+      console.log(this.oddNumbers);
     }
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // tag name
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
-
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-      console.log(this.evenNumbers);
-    } else {
-      this.oddNumbers.push(firedNumber);
-      console.log(this.oddNumbers);
-    }
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
