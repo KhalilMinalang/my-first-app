bc0bc873f5cd0b47ef097953b8af13e2c2ba7466
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-01 15:55:46 +0800

    Completed: 145 Outsourcing the Route Configuration

---
+++ src/app/app-route.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+
+const appRoutes: Routes = [
+  {
+    path: '', // localhost:4200/
+    component: HomeComponent,
+  },
+  {
+    path: 'users', // localhost:4200/users
+    component: UsersComponent,
+    children: [
+      // passing a parameter
+      {
+        path: ':id/:name',
+        component: UserComponent,
+      },
+    ],
+  },
+
+  {
+    path: 'servers', // localhost:4200/users
+    component: ServersComponent,
+    children: [
+      {
+        path: ':id',
+        component: ServerComponent,
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+      },
+    ],
+  },
+
+  // page not found
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  // wild card route MUST BE AT THE END
+  {
+    path: '**',
+    redirectTo: '/not-found',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  // declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,51 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  {
-    path: '', // localhost:4200/
-    component: HomeComponent,
-  },
-  {
-    path: 'users', // localhost:4200/users
-    component: UsersComponent,
-    children: [
-      // passing a parameter
-      {
-        path: ':id/:name',
-        component: UserComponent,
-      },
-    ],
-  },
-
-  {
-    path: 'servers', // localhost:4200/users
-    component: ServersComponent,
-    children: [
-      {
-        path: ':id',
-        component: ServerComponent,
-      },
-      {
-        path: ':id/edit',
-        component: EditServerComponent,
-      },
-    ],
-  },
-
-  // page not found
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent,
-  },
-  // wild card route MUST BE AT THE END
-  {
-    path: '**',
-    redirectTo: '/not-found',
-    pathMatch: 'full',
-  },
-];
+import { AppRoutingModule } from './app-route.module';
 
 @NgModule({
   declarations: [
@@ -69,11 +24,7 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes), // pass appRoutes as an argument
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
