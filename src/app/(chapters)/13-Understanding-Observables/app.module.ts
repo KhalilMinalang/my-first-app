91c32d2b8826a5c2d55f2d267c8fc63f5e496ba8
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-03 14:42:24 +0800

    Completed: 186 An Example Form

--- src/app/(chapters)/13-Understanding-Observables/app.module.ts
+++ src/app/(chapters)/13-Understanding-Observables/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HomeComponent, UserComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub!: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(
-      (didActivate: boolean) => {
-        this.userActivated = didActivate;
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
