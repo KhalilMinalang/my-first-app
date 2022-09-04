7b10cbdf157446d415b8e1f5c6b176be7ec15e0c
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-04 16:55:03 +0800

    Completed: 196 TD_ Using ngModel with Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') signUpForm!: NgForm;
   // defaultQuestion = 'pet';
   defaultQuestion = 'teacher';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
