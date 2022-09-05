b7d412de76606f23b5b937e3f74ee10964227222
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-05 10:40:14 +0800

    Completed: 203 Reactive_ Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
