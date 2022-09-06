5807bee2a2cd456aa7a82966419d026a58e996b6
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-06 18:18:59 +0800

    Completed: 217 [assignment_solution] Practicing Reactive Forms - MY SOLUTION

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AsyncValidatorFn,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  projectStatuses = ['stable', 'critical', 'finished'];
+  projectForm!: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        Validators.required,
+        this.forbiddenName as AsyncValidatorFn
+      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl(this.projectStatuses[0]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  forbiddenName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ nameIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // must be imported for [(ngModel)] directive
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
