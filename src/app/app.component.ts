67dc6afb385f0ae0866ca446c38c60dd473ae21d
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-08 07:37:15 +0800

    Completed: 241 Introduction & Why Pipes are Useful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  AsyncValidatorFn,
-  ValidatorFn,
-} from '@angular/forms';
-// import { Observable } from 'rxjs';
-import { CustomValidators } from './custom-validators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  // projectStatuses = ['stable', 'critical', 'finished'];
-  projectForm!: FormGroup;
-
-  ngOnInit() {
-    this.projectForm = new FormGroup({
-      name: new FormControl(
-        null,
-        [Validators.required, CustomValidators.invalidProjectName],
-        // this.forbiddenName as AsyncValidatorFn
-        CustomValidators.asyncInvalidProjectName as AsyncValidatorFn
-      ),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      // projectStatus: new FormControl(this.projectStatuses[0]),
-      status: new FormControl('critical'),
-    });
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+  ];
+  getStatusClasses(server: {
+    instanceType: string;
+    name: string;
+    status: string;
+    started: Date;
+  }) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical',
+    };
   }
-
-  onSaveProject() {
-    console.log(this.projectForm.value);
-  }
-
-  // forbiddenName(control: FormControl): Promise<any> | Observable<any> {
-  //   const promise = new Promise<any>((resolve, reject) => {
-  //     setTimeout(() => {
-  //       if (control.value === 'Test') {
-  //         resolve({ nameIsForbidden: true });
-  //       } else {
-  //         resolve(null);
-  //       }
-  //     }, 1500);
-  //   });
-  //   return promise;
-  // }
 }
