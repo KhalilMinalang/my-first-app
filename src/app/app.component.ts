7aa93139993ab86d53e879335d2cb720ad64ccee
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-08 15:34:07 +0800

    Completed: 261 Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-54479-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
@@ -58,6 +60,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
