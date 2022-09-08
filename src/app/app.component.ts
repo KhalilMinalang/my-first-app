4bb1bdba67c0a24b774a630e5ccf216cc2ff851f
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-08 13:21:49 +0800

    Completed: 257 GETting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -27,9 +29,20 @@ export class AppComponent {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://ng-complete-guide-54479-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
