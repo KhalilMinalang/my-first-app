2bed8c315235de5f762ea273c2601bc664434e6f
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-09 13:01:55 +0800

    Completed: 276 Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [
-    // depedency injection syntax
+    // depedency injection syntax NOTE: ORDER OF INTERCEPTORS MATTER
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
+      useClass: LoggingInterceptorService, // SECOND
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService, // FIRST
       multi: true,
     },
   ],
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,15 +1,16 @@
 import {
-  HttpEventType,
+  // HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+// import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on its way');
-    console.log(req.url);
+    // console.log('Request is on its way');
+    // console.log(req.url);
+
     // you can work some magic here
     // if (req.) {}
 
@@ -22,20 +23,17 @@ export class AuthInterceptorService implements HttpInterceptor {
     });
 
     // return next.handle(req);
-    return (
-      next
-        .handle(modfiedRequest)
-        // Response Interceptors
-        // Here you can also use other operators map()
-        .pipe(
-          tap((event) => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-              console.log('Response arrived, body data: ');
-              console.log(event.body);
-            }
-          })
-        )
-    );
+    return next.handle(modfiedRequest);
+    // Response Interceptors
+    // Here you can also use other operators map()
+    // .pipe(
+    //   tap((event) => {
+    //     console.log(event);
+    //     if (event.type === HttpEventType.Response) {
+    //       console.log('Response arrived, body data: ');
+    //       console.log(event.body);
+    //     }
+    //   })
+    // )
   }
 }
