0556ad2e60c20900a46a90ec4437f696850d0d9f
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-09 09:09:54 +0800

    Completed: 270 Adding Query Params

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
@@ -28,11 +28,20 @@ export class PostsService {
   }
 
   fetchPosts() {
+    // multiple params
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+
     return this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-54479-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
         {
           headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          // one param
+          // params: new HttpParams().set('print', 'pretty'),
+          // multiple params
+          params: searchParams,
         }
       )
       .pipe(
