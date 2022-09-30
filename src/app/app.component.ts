e7ff7169406d7339aaeef698be1e191ad7caf629
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-30 08:23:33 +0800

    Completed: 420 Advanced Transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,23 +29,61 @@ import { Component } from '@angular/core';
           transform: 'translateX(100px)',
         })
       ),
+      // transition('normal => highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800)),
+      // previous 2 lines in one
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          // backgroundColor
+          'background-color': 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          // backgroundColor: 'blue',
+          'background-color': 'blue',
+          transform: 'translateX(100px)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          'background-color': 'green',
+          transform: 'translateX(0) scale(0.5)',
+        })
+      ),
+      // transition('normal <=> highlighted', animate(300)),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500)),
     ]),
   ],
 })
 export class AppComponent {
   state = 'normal';
   // state = 'highlighted';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal'
       ? (this.state = 'highlighted')
       : (this.state = 'normal');
+
+    this.wildState == 'normal'
+      ? (this.wildState = 'highlighted')
+      : (this.wildState = 'normal');
   }
 
-  onShrink() {}
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
   onAdd(item: any) {
     this.list.push(item);
