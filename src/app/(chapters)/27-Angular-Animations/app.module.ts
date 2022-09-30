63c7f43f3bae7d6363c37f14f29a494a281dae20
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-30 10:14:51 +0800

    Completed: 426 Module Introduction

--- src/app/(chapters)/27-Angular-Animations/app.module.ts
+++ src/app/(chapters)/27-Angular-Animations/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
