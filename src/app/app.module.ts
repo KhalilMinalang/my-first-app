594ad77727a1e647af81b21303bffc94105804ae
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-30 12:49:13 +0800

    Completed: 434 Analyzing the Testing Setup (as created by the CLI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  declarations: [AppComponent, UserComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
