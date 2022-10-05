b7d0ae8bd743ddaa71aeded8ccff2c0bb797ef40
KhalilMinalang <minalang.khalil@gmail.com>
2022-10-05 13:28:29 +0800

    Completed: 460 Module Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component, Injector } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
-import { DomSanitizer } from '@angular/platform-browser';
-
-import { AlertComponent } from './alert.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  content!: any;
-
-  constructor(injector: Injector, domSanitizer: DomSanitizer) {
-    const AlertElement = createCustomElement(AlertComponent, {
-      injector: injector,
-    });
-    customElements.define('my-alert', AlertElement);
-    setTimeout(() => {
-      // this.content = '<p>A paragraph!</p>';
-      this.content = domSanitizer.bypassSecurityTrustHtml(
-        "<my-alert message='Rendered dynamically'></my-alert>"
-      );
-    }, 1000);
-  }
-}
+export class AppComponent {}
