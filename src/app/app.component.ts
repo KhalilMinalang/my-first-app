2cc7dbdc0bfc5b89486c325c1e4ebe50cf6cb833
KhalilMinalang <minalang.khalil@gmail.com>
2022-10-05 13:37:45 +0800

    Completed: 461 What & Why_

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    //
+    function add(a: number, b: number) {
+      return a + b;
+    }
+
+    const result = add(2, 5);
+
+    console.log(result);
+  }
+}
